6cc04c1a57cbe289ce2cba36bca1de652ed4c3f9
danielgitgit <daniel.work.idn@gmail.com>
2023-11-15 17:22:32 +0700

    first commit FE

---
+++ src/components/Loading.tsx
@@ -0,0 +1,18 @@
+const Loading = () => {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto ">
+        <div
+          className="text-blue-600 inline-block h-48 w-48 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-info motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
