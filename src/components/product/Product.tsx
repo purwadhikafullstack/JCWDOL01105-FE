15a2061bfe5d33348156eafabc616b52cb822c9b
mcnwr76 <anwaraan998@gmail.com>
2023-11-28 10:41:08 +0700

    [FPMGJ-21] Feat: Property Detail

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,5 +1,22 @@
-const Product = () => {
-  return <div></div>;
+import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
+import { Link } from "react-router-dom";
+
+interface IData {
+  data: { id: string; name: string; description: string; image_url: string; location: { city: string } };
+}
+
+const Product: React.FC<IData> = ({ data }) => {
+  return (
+    <Card className="w-[300px] flex flex-grow m-2">
+      <Link to={`/property/${data.id}`}>
+        <CardContent className="w-full p-4">
+          <img loading="lazy" className="w-full" src={data.image_url} alt="" />
+          <CardTitle className="my-2">{data.name}</CardTitle>
+          <CardDescription>{data.location.city}</CardDescription>
+        </CardContent>
+      </Link>
+    </Card>
+  );
 };
 
 export default Product;
--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,5 +1,21 @@
+import { useGetAPI } from "@/lib/service";
+import Product from "./Product";
+
+interface IData {
+  id: string;
+  name: string;
+  description: string;
+  image_url: string;
+  location: { city: string };
+}
+
 const ProductList = () => {
-  return <div></div>;
+  const { data: products, isFetched } = useGetAPI("/api/property", "all-property");
+  return (
+    <div className="flex flex-wrap justify-around">
+      {isFetched && products.map((data: IData) => <Product key={data.id} data={data} />)}
+    </div>
+  );
 };
 
 export default ProductList;
