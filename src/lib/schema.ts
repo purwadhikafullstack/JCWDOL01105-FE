8fd30f5dbc8fb9b5697eb03eccb9db32a95ffd20
mcnwr76 <anwaraan998@gmail.com>
2023-11-29 21:59:38 +0700

    [FPMGJ-20] Feat: Seacrh Property

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -147,3 +147,7 @@ export const genderSchema = z.object({
 export const birthdateSchema = z.object({
   birthdate: z.number(),
 });
+
+export const searchSchema = z.object({
+  location: z.any(),
+});
