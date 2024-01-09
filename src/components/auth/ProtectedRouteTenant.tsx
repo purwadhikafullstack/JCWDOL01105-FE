df4325c119f5d8897bd0954ce1043087568b3518
mcnwr76 <anwaraan998@gmail.com>
2024-01-09 11:22:45 +0700

    update

--- src/components/auth/ProtectedRouteTenant.tsx
+++ src/components/auth/ProtectedRouteTenant.tsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRouteTenant = ({ children }: { children: React.ReactNode }) => {
-  const { isLogin, role, id } = useContext(AuthContext);
+  const { isLogin, role } = useContext(AuthContext);
   // const navigate = useNavigate();
   console.log(role);
   if (isLogin && role === "tenant") {
